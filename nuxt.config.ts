057d0f1d41a975213e4a80c6862b3986fd53d1c2
Matheus <matessousa@outlook.com>
2022-05-22 22:56:13 -0300

    chore: added tailwind css reset  and components set  to global

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,4 +4,14 @@ import { defineNuxtConfig } from 'nuxt'
 export default defineNuxtConfig({
     buildModules: ['@unocss/nuxt'],
     modules: ['@nuxtjs/supabase'],
+    unocss: {
+        uno: true, 
+        icons: true,
+        attributify: true, 
+    },
+    css: ['@/assets/css/main.css', '@unocss/reset/tailwind.css'],
+    components: {
+        global: true,
+        dirs: ['~/components']
+    }
 })
